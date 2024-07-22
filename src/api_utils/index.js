d81f3b70be5169312a2fe765397295f66b590a42
zeeshan-algo <zeeshan.ashraf@algorepublic.com>
2024-07-22 12:16:50 +0500

    values added

--- src/api_utils/index.js
+++ src/api_utils/index.js
@@ -1,1 +1,3 @@
-export const API_HOST = "http://localhost:8080/api/";
+// export const API_HOST = "http://localhost:8080/api/";
+export const API_HOST = "https://web-builder-backend-7yo4r6k6r-zeeshan-ashrafs-projects.vercel.app/api/";
+
